eee01fa17849594a3a7c2046a45f3e3fb78d0dac
Philip Persson <kalllez123@hotmail.se>
2018-01-21 20:30:01 +0100

    Overview view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TrudvangCharacter } from '../models/TrudvangCharacter';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor() {
+    this.model = new TrudvangCharacter();
+  }
+
+  model: TrudvangCharacter;
+
+  onSubmit() {
+    console.log(this.model);
+  }
 }
---
+++ src/models/TrudvangCharacter.ts
@@ -0,0 +1,11 @@
+export class TrudvangCharacter {
+    name: string;    
+    race: string;
+    culture: string;
+    religion: string;
+    gender: string;
+    height: number;
+    weight: number;
+    weaponHand: string;
+    background: string;
+}
