76b31a4058f063536658081cf0a085cb49d92986
Philip Persson <philipp.dev@hotmail.com>
2019-01-06 16:06:10 +0100

    Updated packages

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 echo Starting Database
-start cmd.exe /c "mongod --dbpath E:\mongodata\Trudvang"
+start cmd.exe /c "mongod --dbpath D:\Data\Trudvang"
 timeout /t 5
 echo Starting Web-server
 start cmd.exe /c "nodemon server.js"
